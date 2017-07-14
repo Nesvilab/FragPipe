fe44cd8bbaee6aad337fa1c4723fa18b110f7af2
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2017-07-13 22:56:09 -0400

    Bump version to v2.9

--- MSFragger-GUI/src/umich/msfragger/Version.java
+++ MSFragger-GUI/src/umich/msfragger/Version.java
@@ -23,7 +23,7 @@ import javax.swing.JOptionPane;
  */
 public class Version {
     public static final String PROP_VER = "msfragger.gui.version";
-    public static final String VERSION = "2.8.3";
+    public static final String VERSION = "2.9";
     
     public static String getVersion() {
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("umich/msfragger/gui/Bundle"); // NOI18N
