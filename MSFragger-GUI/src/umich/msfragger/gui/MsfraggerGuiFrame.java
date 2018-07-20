ec6292a6d49a700413b8242fc0ff334c1b7bea11
Dmitry Avtonomov <dmt.avt@gmail.com>
2018-07-19 19:20:28 -0700

    Change icon from bolt to fragpipe logo
    It doesn't look good on dark backgrounds, unlike the bolt.

--- MSFragger-GUI/src/umich/msfragger/gui/MsfraggerGuiFrame.java
+++ MSFragger-GUI/src/umich/msfragger/gui/MsfraggerGuiFrame.java
@@ -5225,9 +5225,9 @@ public class MsfraggerGuiFrame extends javax.swing.JFrame {
         // Icon attribution string:
         // <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         List<Image> images = new ArrayList<>();
-        int[] sizes = {16, 24, 32, 64, 128, 256, 512};
+        int[] sizes = {16, 24, 32, 64, 128, 256};
         final String path = "icons/";
-        final String baseName = "bolt-";
+        final String baseName = "fragpipe-";
         final String ext = ".png";
         for (int size : sizes) {
             String location = path + baseName + Integer.toString(size) + ext;
