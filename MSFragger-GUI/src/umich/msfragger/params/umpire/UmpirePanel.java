f7ff9eaf6fe01e8ce4ce4f3bf06fc30e9ba23f07
Dmitry Avtonomov <dmt.avt@gmail.com>
2018-10-05 16:32:37 -0700

    Reformat and update SwingUtils

--- MSFragger-GUI/src/umich/msfragger/params/umpire/UmpirePanel.java
+++ MSFragger-GUI/src/umich/msfragger/params/umpire/UmpirePanel.java
@@ -1,17 +1,18 @@
 package umich.msfragger.params.umpire;
 
-import java.awt.Dimension;
 import java.text.DecimalFormat;
+import java.text.Normalizer.Form;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.DefaultFormatterFactory;
 import javax.swing.text.NumberFormatter;
-import net.miginfocom.layout.AC;
 import net.miginfocom.layout.CC;
 import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
@@ -45,45 +46,80 @@ public class UmpirePanel extends JPanel {
     JPanel pFrag = new JPanel(new MigLayout());
     pFrag.setBorder(new TitledBorder("Fragment grouping options"));
 
-    DefaultFormatterFactory fmtFactoryDecimalAsInt = new DefaultFormatterFactory(
+    DefaultFormatterFactory decimalAsInt = new DefaultFormatterFactory(
         new NumberFormatter(new DecimalFormat("#0")));
-    DefaultFormatterFactory fmtFactoryDecimal = new javax.swing.text.DefaultFormatterFactory(
+    DefaultFormatterFactory decimal = new javax.swing.text.DefaultFormatterFactory(
         new javax.swing.text.NumberFormatter());
 
-    JLabel lblRpMax = new JLabel("RPmax");
-    JLabel lblRfMax = new JLabel("RFMax");
-    JLabel lblCorrThresh = new JLabel("Corr Threshold");
-    JLabel lblDeltaApex = new JLabel("Delta Apex");
+
+
     JLabel lblRtOverlap = new JLabel("RT Overlap");
 
-    JFormattedTextField fmtRpMax = new JFormattedTextField(fmtFactoryDecimalAsInt);
-    JFormattedTextField fmtRfMax = new JFormattedTextField(fmtFactoryDecimal);
-    JFormattedTextField fmtCorrThresh = new JFormattedTextField(fmtFactoryDecimal);
-    JFormattedTextField fmtDeltaApex = new JFormattedTextField(fmtFactoryDecimal);
-    JFormattedTextField fmtRtOverlap = new JFormattedTextField(fmtFactoryDecimal);
+    FormEntry feRpMax = new FormEntry(UmpireParams.PROP_RPmax, "RP max", new JFormattedTextField(decimalAsInt));
+    FormEntry feRfMax = new FormEntry(UmpireParams.PROP_RFmax, "RF max", new JFormattedTextField(decimal));
+    FormEntry feCorrThresh = new FormEntry(UmpireParams.PROP_CorrThreshold, "Corr Threshold", new JFormattedTextField(decimal));
+    FormEntry feDeltaApex = new FormEntry(UmpireParams.PROP_DeltaApex, "Delta Apex", new JFormattedTextField(decimal));
+    FormEntry feRtOverlap = new FormEntry(UmpireParams.PROP_RTOverlap, "RT Overlap", new JFormattedTextField(decimal));
+    //FormEntry fe = new FormEntry(UmpireParams.PROP_, "", );
     JCheckBox checkBoostComplimentaryIons = new JCheckBox("Boost complimentary ions");
     JCheckBox checkAdjustFragIntensity = new JCheckBox("Adjust fragment intensity");
 
     CC ccFmt = new CC().width("30:50:70px");
     CC ccFmtWrap = new CC().width("30:50:70px").wrap();
     CC ccLbl = new CC().alignX("right").gapBefore("5px");
-    pFrag.add(lblRpMax, ccLbl);
-    pFrag.add(fmtRpMax, ccFmt);
-    pFrag.add(lblRfMax, ccLbl);
-    pFrag.add(fmtRfMax, ccFmt);
-    pFrag.add(lblCorrThresh, ccLbl);
-    pFrag.add(fmtCorrThresh, ccFmt);
-    pFrag.add(lblDeltaApex, ccLbl);
-    pFrag.add(fmtDeltaApex, ccFmt);
-    pFrag.add(lblRtOverlap, ccLbl);
-    pFrag.add(fmtRtOverlap, ccFmtWrap);
+    pFrag.add(feRpMax.label(), ccLbl);
+    pFrag.add(feRpMax.comp, ccFmt);
+    pFrag.add(feRfMax.label(), ccLbl);
+    pFrag.add(feRfMax.comp, ccFmt);
+    pFrag.add(feCorrThresh.label(), ccLbl);
+    pFrag.add(feCorrThresh.comp, ccFmt);
+    pFrag.add(feDeltaApex.label(), ccLbl);
+    pFrag.add(feDeltaApex.comp, ccFmt);
+    pFrag.add(feRtOverlap.label(), ccLbl);
+    pFrag.add(feRtOverlap.comp, ccFmtWrap);
     pFrag.add(checkBoostComplimentaryIons, "span, split 2");
     pFrag.add(checkAdjustFragIntensity);
 
 
+    // Panel - fragment grouping options
+    List<FormEntry> entries = new ArrayList<>();
+    //entries.add(new FormEntry(UmpireParams.PROP_, "", new JFormattedTextField()));
+    entries.add(new FormEntry(UmpireParams.PROP_MS1PPM, "MS1 PPM", new JFormattedTextField(decimalAsInt)));
+    entries.add(new FormEntry(UmpireParams.PROP_MS2PPM, "MS2 PPM", new JFormattedTextField(decimalAsInt)));
+    entries.add(new FormEntry(UmpireParams.PROP_MaxCurveRTRange, "Max Curve RT Range", new JFormattedTextField(decimal)));
+
+    entries.add(new FormEntry(UmpireParams.PROP_MinMSIntensity, "Min MS1 Intensity", new JFormattedTextField(decimal)));
+    entries.add(new FormEntry(UmpireParams.PROP_MinMSMSIntensity, "Min MS2 Intensity", new JFormattedTextField(decimal)));
+    entries.add(new FormEntry(UmpireParams.PROP_MinFrag, "Min Fragments", new JFormattedTextField(decimalAsInt)));
+
+    entries.add(new FormEntry(UmpireParams.PROP_SN, "MS1 S/N", new JFormattedTextField(decimalAsInt)));
+    entries.add(new FormEntry(UmpireParams.PROP_MS2SN, "MS2 S/N", new JFormattedTextField(decimalAsInt)));
+    entries.add(new FormEntry(UmpireParams.PROP_EstimateBG, "Estimate Background", new JCheckBox()));
+
+    entries.add(new FormEntry(UmpireParams.PROP_, "", new JFormattedTextField()));
+    entries.add(new FormEntry(UmpireParams.PROP_, "", new JFormattedTextField()));
+    entries.add(new FormEntry(UmpireParams.PROP_, "", new JFormattedTextField()));
+    //entries.add(new FormEntry(UmpireParams.PROP_, "", new JFormattedTextField()));
+
 
     this.add(pTop, new CC().growX());
     this.add(pFrag, new CC().growX());
+  }
+
+  private static class FormEntry {
+    JComponent comp;
+    String propName;
+    String label;
+
+    public FormEntry(String propName, String label, JComponent comp) {
+      this.propName = propName;
+      this.label = label;
+      this.comp = comp;
+    }
+
+    JLabel label() {
+      return new JLabel(label);
+    }
   }
 
   public UmpireParams collectUmpireParams() {
--- MSFragger-GUI/src/umich/msfragger/util/SwingUtils.java
+++ MSFragger-GUI/src/umich/msfragger/util/SwingUtils.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2018 Dmitry Avtonomov
  *
  * This program is free software: you can redistribute it and/or modify
@@ -28,12 +28,17 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JCheckBox;
 import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -45,193 +50,231 @@ import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
-import umich.msfragger.gui.MsfraggerGuiFrame;
 
 /**
- *
  * @author Dmitry Avtonomov
  */
 public class SwingUtils {
+  private SwingUtils() {}
 
-    public static Component findParentComponentForDialog(Component origin) {
-        if (origin == null) {
-            return null;
-        }
-        Container parent = origin.getParent();
-        if (parent instanceof JFrame) {
-            return parent;
-        }
-        return findParentComponentForDialog(parent);
+  public static String getStrVal(Component c) {
+
+    String val = null;
+    if (c instanceof JFormattedTextField) {
+      val = ((JFormattedTextField) c).getText();
+    } else if (c instanceof JTextField) {
+      val = ((JTextField) c).getText();
+    } else if (c instanceof JSpinner) {
+      val = ((JSpinner) c).getValue().toString();
+    } else if (c instanceof JCheckBox) {
+      val = Boolean.valueOf(((JCheckBox) c).isSelected()).toString();
     }
 
-    private SwingUtils() {}
-    
-    public static void setFileChooserPath(JFileChooser fileChooser, String path) {
-        if (path == null) {
-            fileChooser.setCurrentDirectory(null);
-            return;
-        }
-        Path p = Paths.get(path);
-        if (Files.exists(p)) {
-            fileChooser.setCurrentDirectory(p.toFile());
-        } else {
-            fileChooser.setCurrentDirectory(null);
-        }
+    return val.trim();
+  }
+
+  public static Map<String, Component> mapComponentsByName(Container origin,
+      boolean includeOrigin) {
+    if (origin == null) {
+      return Collections.emptyMap();
     }
-    
-    
-    public static void enableComponents(Container container, boolean enable) {
-        Component[] components = container.getComponents();
-        for (Component component : components) {
-            component.setEnabled(enable);
+    Map<String, Component> map = new HashMap<>();
+    ArrayDeque<Component> fifo = new ArrayDeque<>();
+    synchronized (origin.getTreeLock()) {
+      if (includeOrigin) {
+        fifo.addLast(origin);
+      } else {
+        for (Component c : origin.getComponents()) {
+          fifo.addLast(c);
+        }
+      }
+      while (!fifo.isEmpty()) {
+        Component c = fifo.removeFirst();
+        String name = c.getName();
+        if (!StringUtils.isNullOrWhitespace(name)) {
+          map.put(name, c);
+        }
+        if (c instanceof Container) {
+          fifo.addLast(c);
+        }
+      }
+    }
+    return map;
+  }
+
+  public static Component findParentComponentForDialog(Component origin) {
+    if (origin == null) {
+      return null;
+    }
+    Container parent = origin.getParent();
+    if (parent instanceof JFrame) {
+      return parent;
+    }
+    return findParentComponentForDialog(parent);
+  }
+
+  public static void setFileChooserPath(JFileChooser fileChooser, String path) {
+    if (path == null) {
+      fileChooser.setCurrentDirectory(null);
+      return;
+    }
+    Path p = Paths.get(path);
+    if (Files.exists(p)) {
+      fileChooser.setCurrentDirectory(p.toFile());
+    } else {
+      fileChooser.setCurrentDirectory(null);
+    }
+  }
+
+  public static void enableComponents(Container container, boolean enable) {
+    Component[] components = container.getComponents();
+    for (Component component : components) {
+      component.setEnabled(enable);
 //            if (component instanceof JScrollPane) {
 //                JScrollPane jsp = (JScrollPane)component;
 //                enableComponents(jsp.getViewport(), enable);
 //            }
-            if (component instanceof Container) {
-                enableComponents((Container)component, enable);
-            }
-        }
+      if (component instanceof Container) {
+        enableComponents((Container) component, enable);
+      }
     }
-    
-    /**
-     * Installs a listener to receive notification when the text of any
-     * {@code JTextComponent} is changed. Internally, it installs a
-     * {@link DocumentListener} on the text component's {@link Document}, and a
-     * {@link PropertyChangeListener} on the text component to detect if the
-     * {@code Document} itself is replaced.
-     *
-     * @param text any text component, such as a {@link JTextField} or
-     * {@link JTextArea}
-     * @param changeListener a listener to receieve {@link ChangeEvent}s when
-     * the text is changed; the source object for the events will be the text
-     * component
-     * @throws NullPointerException if either parameter is null
-     * 
-     * Taken from http://stackoverflow.com/questions/3953208/value-change-listener-to-jtextfield
-     * 
-     * @author Boann
-     */
-    public static void addChangeListener(final JTextComponent text, final ChangeListener changeListener) {
-        if (text == null || changeListener == null)
-            throw new IllegalArgumentException("Both the text component and the change listener need to be non-null");
-                
-        
-        final DocumentListener dl = new DocumentListener() {
-            private int lastChange = 0, lastNotifiedChange = 0;
+  }
 
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                changedUpdate(e);
-            }
+  /**
+   * Installs a listener to receive notification when the text of any {@code JTextComponent} is
+   * changed. Internally, it installs a {@link DocumentListener} on the text component's {@link
+   * Document}, and a {@link PropertyChangeListener} on the text component to detect if the {@code
+   * Document} itself is replaced.
+   *
+   * @param text any text component, such as a {@link JTextField} or {@link JTextArea}
+   * @param changeListener a listener to receieve {@link ChangeEvent}s when the text is changed; the
+   * source object for the events will be the text component
+   * @throws NullPointerException if either parameter is null
+   *
+   * Taken from http://stackoverflow.com/questions/3953208/value-change-listener-to-jtextfield
+   * @author Boann
+   */
+  public static void addChangeListener(final JTextComponent text,
+      final ChangeListener changeListener) {
+    if (text == null || changeListener == null) {
+      throw new IllegalArgumentException(
+          "Both the text component and the change listener need to be non-null");
+    }
 
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                changedUpdate(e);
-            }
+    final DocumentListener dl = new DocumentListener() {
+      private int lastChange = 0, lastNotifiedChange = 0;
 
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                lastChange++;
-                
-                Runnable runnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        if (lastNotifiedChange != lastChange) {
-                            lastNotifiedChange = lastChange;
-                            changeListener.stateChanged(new ChangeEvent(text));
-                        }
-                    }
-                };
-                
-                SwingUtilities.invokeLater(runnable);
+      @Override
+      public void insertUpdate(DocumentEvent e) {
+        changedUpdate(e);
+      }
+
+      @Override
+      public void removeUpdate(DocumentEvent e) {
+        changedUpdate(e);
+      }
+
+      @Override
+      public void changedUpdate(DocumentEvent e) {
+        lastChange++;
+
+        Runnable runnable = new Runnable() {
+          @Override
+          public void run() {
+            if (lastNotifiedChange != lastChange) {
+              lastNotifiedChange = lastChange;
+              changeListener.stateChanged(new ChangeEvent(text));
             }
+          }
         };
-   
-        PropertyChangeListener pcl = new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent e) {
-                Document d1 = (Document) e.getOldValue();
-                Document d2 = (Document) e.getNewValue();
-                if (d1 != null) {
-                    d1.removeDocumentListener(dl);
-                }
-                if (d2 != null) {
-                    d2.addDocumentListener(dl);
-                }
-                dl.changedUpdate(null);
+
+        SwingUtilities.invokeLater(runnable);
+      }
+    };
+
+    PropertyChangeListener pcl = new PropertyChangeListener() {
+      @Override
+      public void propertyChange(PropertyChangeEvent e) {
+        Document d1 = (Document) e.getOldValue();
+        Document d2 = (Document) e.getNewValue();
+        if (d1 != null) {
+          d1.removeDocumentListener(dl);
+        }
+        if (d2 != null) {
+          d2.addDocumentListener(dl);
+        }
+        dl.changedUpdate(null);
+      }
+    };
+    text.addPropertyChangeListener("document", pcl);
+
+    Document d = text.getDocument();
+    if (d != null) {
+      d.addDocumentListener(dl);
+    }
+  }
+
+  /**
+   * Creates a non-editable JEditorPane that has the same styling as default JLabels and with
+   * hyperlinks clickable. They will be opened in the system default browser.
+   *
+   * @param text Your text to be displayed in HTML context. Don't add the opening and closing HTML
+   * tags. To include links use the regular A tags.
+   * @param addDefaultHyperlinkHandler If true, will add a handler for all hyperlinks to be opened
+   * in the default system browser.
+   */
+  public static JEditorPane createClickableHtml(String text, boolean addDefaultHyperlinkHandler) {
+    // for copying style
+    JLabel label = new JLabel();
+    Font font = label.getFont();
+
+    // create some css from the label's font
+    StringBuilder style = new StringBuilder("font-family:" + font.getFamily() + ";");
+    style.append("font-weight:").append(font.isBold() ? "bold" : "normal").append(";");
+    style.append("font-size:").append(font.getSize()).append("pt;");
+
+    JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">"
+        + text
+        + "</body></html>");
+    ep.setEditable(false);
+
+    // handle link events
+    if (addDefaultHyperlinkHandler) {
+      ep.addHyperlinkListener(new HyperlinkListener() {
+        @Override
+        public void hyperlinkUpdate(HyperlinkEvent e) {
+          if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+            try {
+              openBrowserOrThrow(e.getURL().toURI());
+            } catch (URISyntaxException ex) {
+              throw new IllegalStateException("Incorrect url/uri", ex);
             }
-        };
-        text.addPropertyChangeListener("document", pcl);
-        
-        Document d = text.getDocument();
-        if (d != null) {
-            d.addDocumentListener(dl);
-        }
-    }
-    
-    /**
-     * Creates a non-editable JEditorPane that has the same styling as default
-     * JLabels and with hyperlinks clickable. They will be opened in the system
-     * default browser.
-     * @param text Your text to be displayed in HTML context. Don't add the 
-     * opening and closing HTML tags. To include links use the regular A tags.
-     * @param addDefaultHyperlinkHandler If true, will add a handler for all hyperlinks
-     * to be opened in the default system browser.
-     * @return 
-     */
-    public static JEditorPane createClickableHtml(String text, boolean addDefaultHyperlinkHandler)  {
-        // for copying style
-        JLabel label = new JLabel();
-        Font font = label.getFont();
 
-        // create some css from the label's font
-        StringBuilder style = new StringBuilder("font-family:" + font.getFamily() + ";");
-        style.append("font-weight:").append(font.isBold() ? "bold" : "normal").append(";");
-        style.append("font-size:").append(font.getSize()).append("pt;");
+          }
+        }
+      });
+    }
 
-        JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">"
-            + text
-            + "</body></html>");
-        ep.setEditable(false);
-        
-        // handle link events
-        if (addDefaultHyperlinkHandler) {
-            ep.addHyperlinkListener(new HyperlinkListener()
-            {
-                @Override
-                public void hyperlinkUpdate(HyperlinkEvent e) {
-                    if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
-                        try {
-                            openBrowserOrThrow(e.getURL().toURI());
-                        } catch (URISyntaxException ex) {
-                            throw new IllegalStateException("Incorrect url/uri", ex);
-                        }
-                        
-                    }
-                }
-            });
-        }
-        
-        return ep;
+    return ep;
+  }
+
+  public static void openBrowserOrThrow(URI uri) {
+    try {
+      Desktop.getDesktop().browse(uri);
+    } catch (IOException ex) {
+      throw new IllegalStateException("Could not open link in default system browser", ex);
     }
-    
-    public static void openBrowserOrThrow(URI uri) {
-        try {
-            Desktop.getDesktop().browse(uri);
-        } catch (IOException ex) {
-            throw new IllegalStateException("Could not open link in default system browser", ex);
-        }
-    }
-    
-    /**
-     * Creates a non-editable JEditorPane that has the same styling as default
-     * JLabels. Hyperlink clicks are not handled, user {@link JEditorPane#addHyperlinkListener(javax.swing.event.HyperlinkListener) }
-     * @param text Your text to be displayed in HTML context. Don't add the 
-     * opening and closing HTML tags. To include links use the regular A tags.
-     * @return 
-     */
-    public static JEditorPane createClickableHtml(String text)  {
-        return createClickableHtml(text, true);
-    }
+  }
+
+  /**
+   * Creates a non-editable JEditorPane that has the same styling as default JLabels. Hyperlink
+   * clicks are not handled, user {@link JEditorPane#addHyperlinkListener(javax.swing.event.HyperlinkListener)
+   * }
+   *
+   * @param text Your text to be displayed in HTML context. Don't add the opening and closing HTML
+   * tags. To include links use the regular A tags.
+   */
+  public static JEditorPane createClickableHtml(String text) {
+    return createClickableHtml(text, true);
+  }
 }
