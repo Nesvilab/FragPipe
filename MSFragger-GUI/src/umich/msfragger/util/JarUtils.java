136f17a54811ae50d8c2b32176f11072a914d4b7
Dmitry Avtonomov <dmt.avt@gmail.com>
2018-10-12 15:21:03 -0700

    JarUtils for copying to a subfolder in temp. Add VSCode to ignore.

--- MSFragger-GUI/src/umich/msfragger/util/JarUtils.java
+++ MSFragger-GUI/src/umich/msfragger/util/JarUtils.java
@@ -27,8 +27,9 @@ public class JarUtils {
      * Unpack and possibly rename a file from a jar where a class is loaded from to a temp dir.
      * @param clazz The jar from which to unpack is determined by this class.
      * @param resourceLocation Location of the resource within the jar.
-     * @param randomizeName
-     * @param scheduleForDeletion
+     * @param randomizeName Make the name unique, even if there are similarly named files in the
+     * temp directory.
+     * @param scheduleForDeletion The file will be scheduled for deletion before JVM stops.
      * @return Path to unpacked file.
      * @throws IOException
      */
@@ -36,17 +37,46 @@ public class JarUtils {
         boolean randomizeName, boolean scheduleForDeletion) throws IOException {
 
         try (InputStream in = clazz.getResourceAsStream(resourceLocation)) {
-            Path tempFile;
-            final String resourceName = StringUtils.afterLastChar(resourceLocation, '/', false);
-            String resourceNameMod = resourceName.toLowerCase().endsWith(JAR_FILE_AS_RESOURCE_EXT)
-                ? StringUtils.upToLastDot(resourceLocation) + ".jar"
-                : resourceName;
+            final String resourceNameDest = computeFinalResourceName(resourceLocation);
 
-            if (randomizeName) {
-                tempFile = Files.createTempFile("fragpipe-", "-" + resourceNameMod);
-            } else {
-                tempFile = Paths.get(ThisAppProps.TEMP_DIR, resourceNameMod);
+            Path tempFile = randomizeName
+                ? Files.createTempFile("fragpipe-", "-" + resourceNameDest)
+                : Paths.get(ThisAppProps.TEMP_DIR, resourceNameDest);
+
+            Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
+            if (scheduleForDeletion)
+                tempFile.toFile().deleteOnExit();
+            return tempFile;
+        }
+
+    }
+
+    /**
+     * Unpack and possibly rename a file from a jar where a class is loaded from to a temp dir.
+     * @param clazz The jar from which to unpack is determined by this class.
+     * @param resourceLocation Location of the resource within the jar.
+     * @param locationInTemp Additional nested directories inside the system temp dir.
+     * @param scheduleForDeletion The file will be scheduled for deletion before JVM stops.
+     * @return Path to unpacked file.
+     * @throws IOException
+     */
+    public static Path unpackFromJar(Class<?> clazz, String resourceLocation, Path locationInTemp,
+        boolean scheduleForDeletion) throws IOException {
+
+        try (InputStream in = clazz.getResourceAsStream(resourceLocation)) {
+            final String resourceNameDest = computeFinalResourceName(resourceLocation);
+
+            Path tempDir = Paths.get(ThisAppProps.TEMP_DIR);
+            Path destDir = tempDir.resolve(locationInTemp);
+            Path tempFile = destDir.resolve(resourceNameDest);
+
+            if (Files.notExists(destDir)) {
+                destDir = Files.createDirectories(destDir);
+                if (scheduleForDeletion) {
+                    destDir.toFile().deleteOnExit();
+                }
             }
+
             Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
             if (scheduleForDeletion)
                 tempFile.toFile().deleteOnExit();
@@ -54,4 +84,10 @@ public class JarUtils {
         }
     }
 
+    private static String computeFinalResourceName(String resourceLocation) {
+        final String resourceNameOrig = StringUtils.afterLastChar(resourceLocation, '/', false);
+        return resourceNameOrig.toLowerCase().endsWith(JAR_FILE_AS_RESOURCE_EXT)
+            ? StringUtils.upToLastDot(resourceLocation) + ".jar"
+            : resourceNameOrig;
+    }
 }
