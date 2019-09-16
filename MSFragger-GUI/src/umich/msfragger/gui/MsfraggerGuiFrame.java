5d3bcfb3d39750cae4ec6b59a57bb19a3b8bbcda
Dmitry Avtonomov <dmt.avt@gmail.com>
2019-09-16 16:01:21 -0700

    Move pbiToRunnable()

--- MSFragger-GUI/src/umich/msfragger/gui/MsfraggerGuiFrame.java
+++ MSFragger-GUI/src/umich/msfragger/gui/MsfraggerGuiFrame.java
@@ -81,7 +81,6 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.logging.Level;
@@ -3985,7 +3984,7 @@ public class MsfraggerGuiFrame extends javax.swing.JFrame {
     // run everything
     List<RunnableDescription> toRun = new ArrayList<>();
     for (final ProcessBuilderInfo pbi : pbis) {
-      Runnable runnable = pbiToRunnable(pbi, wdPath, this::printProcessDescription);
+      Runnable runnable = ProcessBuilderInfo.toRunnable(pbi, wdPath, this::printProcessDescription);
       Builder b = new Builder().setName(pbi.name);
       if (pbi.pb.directory() != null) {
         b.setWorkDir(pbi.pb.directory().toString());
@@ -4027,95 +4026,6 @@ public class MsfraggerGuiFrame extends javax.swing.JFrame {
     LogUtils.println(console, "");
     final String cmd = org.apache.commons.lang3.StringUtils.join(pbi.pb.command(), " ");
     LogUtils.print(COLOR_CMDLINE, console, true, cmd, true);
-  }
-
-  public static Runnable pbiToRunnable(final ProcessBuilderInfo pbi, final Path wdPath,
-      Consumer<ProcessBuilderInfo> pbiPrinter) {
-    return () -> {
-
-            final ProcessResult pr = new ProcessResult(pbi);
-            Process started;
-            try {
-              log.debug("Starting: {}", pbi.name);
-              if (pbiPrinter != null) {
-                pbiPrinter.accept(pbi);
-              }
-              started = pr.start();
-              log.debug("Started: {}", pbi.name);
-            } catch (IOException e) {
-              log.error("Error while starting process: " + pbi.name + ", stopping", e);
-              EventBus.getDefault().post(new MessageKillAll());
-              return;
-            }
-
-            // main loop reading process' output
-            try {
-              while (true) {
-
-                Thread.sleep(200L);
-                final byte[] pollErr = pr.pollStdErr();
-                final String errStr = pr.appendErr(pollErr);
-                if (errStr != null) {
-                  EventBus.getDefault().post(new MessageExternalProcessOutput(true, errStr,
-                      pbi.name));
-                }
-                final byte[] pollOut = pr.pollStdOut();
-                final String outStr = pr.appendOut(pollOut);
-                if (outStr != null) {
-                  EventBus.getDefault().post(new MessageExternalProcessOutput(false, outStr,
-                      pbi.name));
-                }
-                if (started.isAlive()) {
-                  continue;
-                }
-
-                try {
-                  log.debug("Checking exit value: {}", pbi.name);
-                  final int exitValue = started.exitValue();
-                  log.debug("Exit value '{}': {}", exitValue, pbi.name);
-                  Color c = exitValue == 0
-                      ? MsfraggerGuiFrame.COLOR_GREEN_DARKER
-                      : MsfraggerGuiFrame.COLOR_RED;
-                  String msg = String.format(Locale.ROOT,
-                      "Process '%s' finished, exit code: %d\n", pbi.name, exitValue);
-                  EventBus.getDefault().post(new MessageAppendToConsole(msg, c));
-                  if (exitValue != 0) {
-                    log.debug("Exit value not zero, killing all processes");
-                    EventBus.getDefault().post(new MessageAppendToConsole(
-                        "Process returned non-zero exit code, stopping", MsfraggerGuiFrame.COLOR_RED));
-                    EventBus.getDefault().post(new MessageKillAll());
-                  }
-
-                } catch (IllegalThreadStateException ex) {
-                  log.warn("Checking for exit value when subprocess was not alive threw exception.");
-                }
-                break;
-              }
-
-            } catch (IOException e) {
-              log.error("Error while starting process " + pbi.name, e);
-
-            } catch (InterruptedException e) {
-              // graceful stop request
-              String msg = "Processing interrupted, stopping " + pbi.name;
-              log.debug(msg, e);
-              EventBus.getDefault()
-                  .post(new MessageAppendToConsole(msg, MsfraggerGuiFrame.COLOR_RED_DARKEST));
-              // all the cleanup is done in the finally block
-
-            } finally {
-              // in the end whatever happens always try to kill the process
-              if (started != null && started.isAlive()) {
-                log.debug("Killing underlying external process");
-                started.destroyForcibly();
-              }
-              try {
-                pr.close();
-              } catch (Exception e) {
-                log.error("Error closing redirected std/err streams from external process", e);
-              }
-            }
-          };
   }
 
   @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
