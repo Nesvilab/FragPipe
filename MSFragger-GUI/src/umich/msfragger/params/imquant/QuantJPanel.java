38d49e7d1c94dd9ca5a0ceef8a355887b8fbdadb
Fengchao <fcyucn@gmail.com>
2019-10-29 14:11:45 -0400

    Fix a typo.

--- MSFragger-GUI/src/umich/msfragger/params/imquant/QuantJPanel.java
+++ MSFragger-GUI/src/umich/msfragger/params/imquant/QuantJPanel.java
@@ -202,7 +202,7 @@ public class QuantJPanel extends JPanelWithEnablement {
         FormEntry feImTol = new FormEntry("ui.imquant.im-tol", "IM Window (1/k0)", uiSpinnerImTol);
         UiSpinnerDouble uiSpinnerMzTol = UiSpinnerDouble.builder(10.0, 1.0, 1000.0, 1.0)
             .setFormat(new DecimalFormat("0.#")).setNumCols(5).create();
-        FormEntry feMzTol = new FormEntry("ui.imquant.mz-tol", "M/z Window (ppm)", uiSpinnerMzTol);
+        FormEntry feMzTol = new FormEntry("ui.imquant.mz-tol", "M/Z Window (ppm)", uiSpinnerMzTol);
         UiCheck uiCheckImquantPlot = new UiCheck("Plot (for debug)", null, false);
         FormEntry fePlot = new FormEntry("ui.imquant.is-plot", "Not shown", uiCheckImquantPlot);
 
