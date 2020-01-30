840ce106887bb5785bb284657606441fe61f5c9d
Dmitry Avtonomov <dmt.avt@gmail.com>
2020-01-30 11:08:11 -0800

    Release 12.2

--- MSFragger-GUI/src/umich/msfragger/Version.java
+++ MSFragger-GUI/src/umich/msfragger/Version.java
@@ -65,7 +65,9 @@ public class Version {
   static {
     CHANGELOG.put("12.2", Arrays.asList(
         "Check FASTA file for presence of decoys before running PeptideProphet or Report",
-        "Fix some Philosopher workspace related bugs"
+        "Fix some Philosopher workspace related bugs",
+        "Spectral library generation update",
+        "IMQuant is now IonQuant"
     ));
     CHANGELOG.put("12.1", Arrays.asList(
             "IMQuant fixes after initial release."
