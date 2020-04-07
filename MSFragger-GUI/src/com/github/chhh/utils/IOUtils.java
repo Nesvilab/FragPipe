f17b19ded3e82ddd6b70a0676a30b7c5793a8df3
Dmitry Avtonomov <dmt.avt@gmail.com>
2020-04-06 17:46:35 -0700

    helper methods to serialize POJOs to string and back

--- MSFragger-GUI/src/com/github/chhh/utils/IOUtils.java
+++ MSFragger-GUI/src/com/github/chhh/utils/IOUtils.java
@@ -17,12 +17,18 @@
 package com.github.chhh.utils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 /**
@@ -65,5 +71,23 @@ public class IOUtils {
             return result;
         }
     }
-    
+
+    /** Read the object from Base64 string. */
+    private static Object fromString( String s ) throws IOException , ClassNotFoundException {
+        byte [] data = Base64.getDecoder().decode( s );
+        ObjectInputStream ois = new ObjectInputStream(
+            new ByteArrayInputStream(  data ) );
+        Object o  = ois.readObject();
+        ois.close();
+        return o;
+    }
+
+    /** Write the object to a Base64 string. */
+    private static String toString( Serializable o ) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream( baos );
+        oos.writeObject( o );
+        oos.close();
+        return Base64.getEncoder().encodeToString(baos.toByteArray());
+    }
 }
