bf4b97d6fffb89fa378fabdddba25d5cc820b167
Dmitry Avtonomov <dmt.avt@gmail.com>
2020-04-30 22:26:13 -0700

    ptmprohet command stub

---
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdPtmProphet.java
@@ -0,0 +1,34 @@
+package com.dmtavt.fragpipe.cmd;
+
+import java.nio.file.Path;
+import org.apache.commons.lang3.NotImplementedException;
+
+public class CmdPtmProphet extends CmdBase {
+  public static String NAME = "PtmProphet";
+
+  public CmdPtmProphet(boolean isRun, Path workDir) {
+    super(isRun, workDir);
+  }
+
+  @Override
+  public String getCmdName() {
+    return NAME;
+  }
+
+  public boolean configure() {
+
+    isConfigured = false;
+
+    if (!isConfigured) {
+      throw new NotImplementedException();
+    }
+
+    isConfigured = true;
+    return true;
+  }
+
+  @Override
+  public int getPriority() {
+    return 93; // after peptide prophet
+  }
+}
