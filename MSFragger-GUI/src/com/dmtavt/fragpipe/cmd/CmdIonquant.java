510f25ecac42c79b19f01f111344fa7bed0ed356
Fengchao <fcyucn@gmail.com>
2020-06-05 11:17:29 -0400

    Update ionquant to 1.3.0.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdIonquant.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdIonquant.java
@@ -25,7 +25,7 @@ public class CmdIonquant extends CmdBase {
   private static final Logger log = LoggerFactory.getLogger(CmdIonquant.class);
 
   public static final String NAME = "IonQuant";
-  public static final String JAR_IONQUANT_NAME = "ionquant-1.3.0-rc1.jar";
+  public static final String JAR_IONQUANT_NAME = "ionquant-1.3.0.jar";
   public static final String JAR_MSFTBX_NAME = ToolingUtils.BATMASS_IO_JAR;
   public static final String JAR_IONQUANT_MAIN_CLASS = "ionquant.IonQuant";
   private static String[] JAR_DEPS = {JAR_MSFTBX_NAME};
