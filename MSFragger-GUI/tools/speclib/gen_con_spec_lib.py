0e119f0a3bbf43107ba8d0bd17f69e305fbcaab6
guoci <zguoci@gmail.com>
2020-06-10 09:00:48 -0400

    [SpecLib] warn for missing `_(un)calibrated.mgf`s

--- MSFragger-GUI/tools/speclib/gen_con_spec_lib.py
+++ MSFragger-GUI/tools/speclib/gen_con_spec_lib.py
@@ -418,6 +418,10 @@ if use_easypqp:
 			spectra_files = mgfs
 	else:
 		spectra_files = [e.resolve(strict=True) for e in spectra_files0 if e.exists()]
+		if all([os.fspath(e).endswith('calibrated.mgf') for e in spectra_files0]):
+			print('Using (un)calibrated.mgfs.')
+			if len(spectra_files) * 2 != len(spectra_files0):
+				raise RuntimeError('missing spectra files')
 		if len(spectra_files) == 0:
 			raise RuntimeError(spectra_files)
 	psm_tsv_file = iproph_RT_aligned / 'psm.tsv'
