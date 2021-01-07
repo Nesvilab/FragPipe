26127b8fe61f56d329ff6096f70d2597be1d2d58
guoci <zguoci@gmail.com>
2021-01-07 14:32:27 -0500

    always use _uncalibrated.mgf for timsTOF data.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdSpecLibGen.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdSpecLibGen.java
@@ -138,7 +138,7 @@ public class CmdSpecLibGen extends CmdBase {
                   final boolean isTimsTOF = dataType == InputDataType.ImMsTimsTof;
                   final String sans_suffix = lcms.getPath().getParent().resolve(fn_sans_extension).toString();
                   final String lcms_path = isTimsTOF ?
-                          sans_suffix + "_calibrated.mgf" + File.pathSeparator + sans_suffix + "_uncalibrated.mgf"
+                          sans_suffix + "_uncalibrated.mgf"
                           :
                           lcms.getPath().toString();
                   return lcms_path;
