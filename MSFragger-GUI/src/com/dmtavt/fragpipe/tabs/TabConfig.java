7f22a0b248c0d9d20cd489c2a7ea55e02a1e72bc
Fengchao <fcyucn@gmail.com>
2021-07-22 16:29:03 -0400

    Set the min required version to 3.3

--- MSFragger-GUI/src/com/dmtavt/fragpipe/tabs/TabConfig.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/tabs/TabConfig.java
@@ -99,7 +99,7 @@ import rx.schedulers.Schedulers;
 public class TabConfig extends JPanelWithEnablement {
 
   private static final Logger log = LoggerFactory.getLogger(TabConfig.class);
-  private static final String msfraggerMinVersion = "3.2";
+  private static final String msfraggerMinVersion = "3.3";
   private static final MigUtils mu = MigUtils.get();
   private static final Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
