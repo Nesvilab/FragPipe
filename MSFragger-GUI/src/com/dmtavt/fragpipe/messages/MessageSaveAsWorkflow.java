2cf791e428e662b6574219b47420a516788f6358
guoci <zguoci@gmail.com>
2021-11-14 17:15:19 -0500

    Add more options for saving and loading workflow and settings

--- MSFragger-GUI/src/com/dmtavt/fragpipe/messages/MessageSaveAsWorkflow.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/messages/MessageSaveAsWorkflow.java
@@ -3,10 +3,16 @@ package com.dmtavt.fragpipe.messages;
 public class MessageSaveAsWorkflow {
   public final boolean toCustomDir;
   public final boolean saveWithFieldTypes;
+  public final boolean saveAll;
 
-  public MessageSaveAsWorkflow(boolean toCustomDir, boolean saveWithFieldTypes) {
+  public MessageSaveAsWorkflow(boolean toCustomDir, boolean saveWithFieldTypes, boolean saveAll) {
     this.toCustomDir = toCustomDir;
     this.saveWithFieldTypes = saveWithFieldTypes;
+    this.saveAll = saveAll;
+  }
+
+  public MessageSaveAsWorkflow(boolean toCustomDir, boolean saveWithFieldTypes) {
+    this(toCustomDir, saveWithFieldTypes, false);
   }
 
   public MessageSaveAsWorkflow(boolean toCustomDir) {
