c2e8d5cc405315044f0220c064583471d89ce924
Fengchao <fcyucn@gmail.com>
2021-12-15 20:48:47 -0500

    Upgrade IonQuant to 1.7.19.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdIonquant.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdIonquant.java
@@ -39,7 +39,7 @@ public class CmdIonquant extends CmdBase {
   private static final Logger log = LoggerFactory.getLogger(CmdIonquant.class);
 
   private static final String NAME = "IonQuant";
-  public static final String JAR_IONQUANT_NAME = "ionquant-1.7.18.jar";
+  public static final String JAR_IONQUANT_NAME = "ionquant-1.7.19.jar";
   private static final String JAR_IONQUANT_MAIN_CLASS = "ionquant.IonQuant";
   private static final String[] JAR_DEPS = {SMILE_CORE_JAR, SMILE_MATH_JAR, JAVACPP_JAR, JAVACPP_WIN_JAR, JAVACPP_LINUX_JAR, OPENBLAS_JAR, OPENBLAS_WIN_JAR, OPENBLAS_LINUX_JAR, BATMASS_IO_JAR};
   private static final List<String> SUPPORTED_FORMATS = Arrays.asList("mzML", "mzXML");
