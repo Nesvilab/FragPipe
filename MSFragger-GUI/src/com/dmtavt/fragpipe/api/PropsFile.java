e8b39f6085948167814b98e1198e1639dc2ebc48
Fengchao <fcyucn@gmail.com>
2021-12-19 14:32:28 -0500

    Reuse save(OuoputStream) in save().

--- MSFragger-GUI/src/com/dmtavt/fragpipe/api/PropsFile.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/api/PropsFile.java
@@ -4,7 +4,6 @@ import com.dmtavt.fragpipe.tabs.TabMsfragger;
 import com.dmtavt.fragpipe.tools.fragger.MsfraggerParams;
 import com.github.chhh.utils.PathUtils;
 import com.github.chhh.utils.PropertiesUtils;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -70,33 +69,13 @@ public class PropsFile extends Properties {
   }
 
   public void save() throws IOException {
-    log.debug("Writing PropsFile to: {}", path.toString());
-    try {
-      PathUtils.createDirs(path.getParent());
-    } catch (IOException ex) {
-      log.error("Could not create directory structure to save properties");
-      throw(ex);
-    }
-    try (OutputStream os = Files.newOutputStream(path)) {
-      //store(os, cacheComments());
-      PropertiesUtils.storeSorted(this, os, comments, true);
-      os.flush();
-    } catch (IOException ex) {
-      log.error("Could not save properties to: " + path.toString(), ex);
-      throw(ex);
-    }
+    PathUtils.createDirs(path.getParent());
+    save(Files.newOutputStream(path));
   }
 
   public void save(OutputStream os) throws IOException {
-    log.debug("Writing properties to stream");
-    try (BufferedOutputStream bos = new BufferedOutputStream(os)) {
-      //store(os, cacheComments());
-      PropertiesUtils.storeSorted(this, os, comments, true);
-      os.flush();
-    } catch (IOException ex) {
-      log.error("Error writing properties to stream", ex);
-      throw(ex);
-    }
+    PropertiesUtils.storeSorted(this, os, comments, true);
+    os.flush();
   }
 
   public Path getPath() {
