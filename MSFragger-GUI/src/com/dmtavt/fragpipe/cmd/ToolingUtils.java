3947020f50c018e6bbd6dedd5f5ff0262b732357
Fengchao <fcyucn@gmail.com>
2022-11-03 22:22:01 -0400

    Upgrade batmass-io to 1.28.4

--- MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/ToolingUtils.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/ToolingUtils.java
@@ -52,7 +52,7 @@ public class ToolingUtils {
   private static final Logger log = LoggerFactory.getLogger(ToolingUtils.class);
   private ToolingUtils() {}
 
-  public static final String BATMASS_IO_JAR = "batmass-io-1.28.0.jar";
+  public static final String BATMASS_IO_JAR = "batmass-io-1.28.4.jar";
   public static final String SMILE_CORE_JAR = "smile-core-2.6.0.jar";
   public static final String SMILE_MATH_JAR = "smile-math-2.6.0.jar";
   public static final String JFREECHART_JAR = "jfreechart-1.5.3.jar";
