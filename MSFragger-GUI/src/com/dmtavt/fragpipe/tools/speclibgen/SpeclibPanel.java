Nesvilab/FragPipe

commit 6c63a23b94d31449cafd866afdfdaa9adae319c3

add `--fragment_types` argument GUI.

MSFragger-GUI/src/com/dmtavt/fragpipe/tools/speclibgen/SpeclibPanel.java
@@ -67,6 +67,12 @@ public class SpeclibPanel extends JPanelBase {
   private UiSpinnerDouble uiSpinnerLowess;
   private UiSpinnerDouble uiSpinner_max_delta_unimod;
   private UiSpinnerDouble uiSpinner_max_delta_ppm;
+  private JCheckBox check_fragment_type_a;
+  private JCheckBox check_fragment_type_b;
+  private JCheckBox check_fragment_type_c;
+  private JCheckBox check_fragment_type_x;
+  private JCheckBox check_fragment_type_y;
+  private JCheckBox check_fragment_type_z;
   private JPanel panelEasypqp;
   private JPanel panelSpectrast;
   public static final String EASYPQP_TIMSTOF = "timsTOF";
@@ -247,8 +253,24 @@ public class SpeclibPanel extends JPanelBase {
         .label("Fragment annotation tol (ppm)")
         .tooltip("Maximum delta mass (PPM) for annotation. [default: 15]").create();
 
+    check_fragment_type_a = new UiCheck("a", null, false);
+    check_fragment_type_b = new UiCheck("b", null, true);
+    check_fragment_type_c = new UiCheck("c", null, false);
+    check_fragment_type_x = new UiCheck("x", null, false);
+    check_fragment_type_y = new UiCheck("y", null, true);
+    check_fragment_type_z = new UiCheck("z", null, false);
+
     mu.add(p, feRadioUseEasypqp.comp);
     mu.add(p, checkKeepIntermediateFiles).wrap();
+    final JPanel p2 = mu.newPanel(mu.lcFillXNoInsetsTopBottom());
+    mu.add(p, new JLabel("fragment types")).wrap();
+    mu.add(p, p2).wrap();
+    mu.add(p2, check_fragment_type_a);
+    mu.add(p2, check_fragment_type_b);
+    mu.add(p2, check_fragment_type_c).wrap();
+    mu.add(p2, check_fragment_type_x);
+    mu.add(p2, check_fragment_type_y);
+    mu.add(p2, check_fragment_type_z);
     mu.add(p, fePqpCal.label(), ccR());
     mu.add(p, fePqpCal.comp).split();
     mu.add(p, labelPqpCalFile);
@@ -406,6 +428,23 @@ public class SpeclibPanel extends JPanelBase {
 
   public double getEasypqp_max_delta_ppm() {
     return uiSpinner_max_delta_ppm.getActualValue();
+  }
+
+  public String getEasypqp_fragment_types() {
+    final boolean[] a = new boolean[]{
+            check_fragment_type_a.isSelected(),
+            check_fragment_type_b.isSelected(),
+            check_fragment_type_c.isSelected(),
+            check_fragment_type_x.isSelected(),
+            check_fragment_type_y.isSelected(),
+            check_fragment_type_z.isSelected(),
+    };
+    final String[] chars = new String[]{"a", "b", "c", "x", "y", "z"};
+    final StringBuilder ret = new StringBuilder("[");
+    for (int i = 0; i < chars.length; i++)
+      if (a[i])
+        ret.append("'").append(chars[i]).append("',");
+    return ret.append("]").toString();
   }
 
   public boolean useSpectrast() {

MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdSpecLibGen.java
@@ -150,9 +150,10 @@ public class CmdSpecLibGen extends CmdBase {
 
         final double max_delta_unimod = speclibPanel.getEasypqp_max_delta_unimod(); // EasyPQP convert
         final double max_delta_ppm = speclibPanel.getEasypqp_max_delta_ppm(); // EasyPQP convert
+        final String fragment_types = speclibPanel.getEasypqp_fragment_types(); // EasyPQP convert
         final double rt_lowess_fraction = speclibPanel.getEasypqpRTLowessFraction(); // EasyPQP library
 
-        cmd.add(OsUtils.asSingleArgument(String.format("--max_delta_unimod %f --max_delta_ppm %f", max_delta_unimod, max_delta_ppm))); // EasyPQP convert args
+        cmd.add(OsUtils.asSingleArgument(String.format("--max_delta_unimod %f --max_delta_ppm %f --fragment_types %s", max_delta_unimod, max_delta_ppm, fragment_types))); // EasyPQP convert args
         cmd.add(OsUtils.asSingleArgument(String.format("--rt_lowess_fraction %f", rt_lowess_fraction))); // EasyPQP library args
 
         final List<String> lcmsfiles = group.lcmsFiles.stream()

commit 4c028b914a8120fe42f5f131556d6c857d98eb33

Fix a bug that does not write EasyPQP fragment types to workflow files.
The bug was introduced by 6c63a23b94d31449cafd866afdfdaa9adae319c3

MSFragger-GUI/src/com/dmtavt/fragpipe/tools/speclibgen/SpeclibPanel.java
@@ -239,11 +239,22 @@ public class SpeclibPanel extends JPanelBase {
         .tooltip("Maximum delta mass (PPM) for annotation. [default: 15]").create();
 
     check_fragment_type_a = new UiCheck("a", null, false);
+    FormEntry feFragmentTypeA = mu.feb(check_fragment_type_a).name("easypqp.fragment.a").label("a").create();
+
     check_fragment_type_b = new UiCheck("b", null, true);
+    FormEntry feFragmentTypeB = mu.feb(check_fragment_type_b).name("easypqp.fragment.b").label("b").create();
+
     check_fragment_type_c = new UiCheck("c", null, false);
+    FormEntry feFragmentTypeC = mu.feb(check_fragment_type_c).name("easypqp.fragment.c").label("c").create();
+
     check_fragment_type_x = new UiCheck("x", null, false);
+    FormEntry feFragmentTypeX = mu.feb(check_fragment_type_x).name("easypqp.fragment.x").label("x").create();
+
     check_fragment_type_y = new UiCheck("y", null, true);
+    FormEntry feFragmentTypeY = mu.feb(check_fragment_type_y).name("easypqp.fragment.y").label("y").create();
+
     check_fragment_type_z = new UiCheck("z", null, false);
+    FormEntry feFragmentTypeZ = mu.feb(check_fragment_type_z).name("easypqp.fragment.z").label("z").create();
 
     mu.add(p, checkKeepIntermediateFiles).wrap();
     mu.add(p, fePqpCal.label(), ccR());
@@ -270,8 +281,8 @@ public class SpeclibPanel extends JPanelBase {
       jLabelAlign.setVisible(false);
       mu.add(p, jLabelAlign);
     }
-    mu.add(p, check_fragment_type_a);
-    mu.add(p, check_fragment_type_x).wrap();
+    mu.add(p, feFragmentTypeA.comp);
+    mu.add(p, feFragmentTypeX.comp).wrap();
     mu.add(p, fe_max_delta_unimod.label(), mu.ccR());
     mu.add(p, fe_max_delta_unimod.comp).split();
     {
@@ -280,8 +291,8 @@ public class SpeclibPanel extends JPanelBase {
       mu.add(p, jLabelAlign);
     }
     mu.add(p, new JLabel(ft));
-    mu.add(p, check_fragment_type_b);
-    mu.add(p, check_fragment_type_y).wrap();
+    mu.add(p, feFragmentTypeB.comp);
+    mu.add(p, feFragmentTypeY.comp).wrap();
     mu.add(p, fe_max_delta_ppm.label(), mu.ccR());
     mu.add(p, fe_max_delta_ppm.comp).split();
     {
@@ -294,8 +305,8 @@ public class SpeclibPanel extends JPanelBase {
       jLabelAlign.setVisible(false);
       mu.add(p, jLabelAlign);
     }
-    mu.add(p, check_fragment_type_c);
-    mu.add(p, check_fragment_type_z).wrap();
+    mu.add(p, feFragmentTypeC.comp);
+    mu.add(p, feFragmentTypeZ.comp).wrap();
 
     uiComboPqpCal.addItemListener(e -> {
       String selected = (String) e.getItem();