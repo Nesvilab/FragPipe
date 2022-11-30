50cf6532f17eb3be46df793a3c9d708a3a206bb0
dpolasky <dpolasky@umich.edu>
2022-11-30 10:43:53 -0500

    add FPOP workflow

--- MSFragger-GUI/src/com/dmtavt/fragpipe/tabs/TabWorkflow.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/tabs/TabWorkflow.java
@@ -252,6 +252,7 @@ public class TabWorkflow extends JPanelWithEnablement {
     builtInWorkflows.add("Nonspecific-HLA-phospho");
     builtInWorkflows.add("Nonspecific-HLA-TMT10");
     builtInWorkflows.add("glyco-O-Pair");
+    builtInWorkflows.add("FPOP");
   }
 
   // Ok, if we could keep some workflows pinned toward the top,  I would say Default, SpecLib, Open, Common-mass-offset, LFQ-MBR,  then the rest
