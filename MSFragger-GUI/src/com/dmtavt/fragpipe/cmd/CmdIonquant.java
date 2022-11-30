f4f90e591d6d75ca7adb6533940c2e06f476b61b
Fengchao <fcyucn@gmail.com>
2022-11-30 15:51:23 -0500

    Fix a rare bug in headless mode.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdIonquant.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdIonquant.java
@@ -87,6 +87,11 @@ public class CmdIonquant extends CmdBase {
 
     List<String> cmd = new ArrayList<>();
     cmd.add(Fragpipe.getBinJava());
+
+    if (Fragpipe.headless) {
+      cmd.add("-Djava.awt.headless=true"); // In some rare case, the server does not have X11 but DISPLAY env var is set, which crashes the headless mode. Setting the headless env to true to prevent the crash.
+    }
+
     cmd.add("-Xmx" + ramGb + "G");
 
     if (extLibsBruker != null) {
