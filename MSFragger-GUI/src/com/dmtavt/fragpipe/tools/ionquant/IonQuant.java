98ab3966f59077c2e4eb79873f61ea8aa929ded3
Fengchao <fcyucn@gmail.com>
2023-01-17 19:15:10 -0500

    Add timeout for MSFragger and IonQuant version check.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/tools/ionquant/IonQuant.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/tools/ionquant/IonQuant.java
@@ -29,6 +29,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.JavaVersion;
@@ -102,7 +103,7 @@ public class IonQuant {
       ProcessBuilder pb = new ProcessBuilder(Fragpipe.getBinJava(), "-jar", jarPath);
       pb.redirectErrorStream(true);
       Process pr = pb.start();
-      pr.waitFor();
+      pr.waitFor(5, TimeUnit.SECONDS);
       try (BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()))) {
         String line;
         while ((line = in.readLine()) != null) {
