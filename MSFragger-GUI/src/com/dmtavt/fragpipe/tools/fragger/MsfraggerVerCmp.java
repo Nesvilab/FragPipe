338d1a3acbd7d171dd551ea9858129da6f001963
Fengchao <fcyucn@gmail.com>
2023-01-27 14:28:57 -0500

    Refine license copyright note.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/tools/fragger/MsfraggerVerCmp.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/tools/fragger/MsfraggerVerCmp.java
@@ -8,11 +8,11 @@
  *
  * FragPipe is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with FragPipe.  If not, see <https://www.gnu.org/licenses/>.
+ * along with FragPipe. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.dmtavt.fragpipe.tools.fragger;
