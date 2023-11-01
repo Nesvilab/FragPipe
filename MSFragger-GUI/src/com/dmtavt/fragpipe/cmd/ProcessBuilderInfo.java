Nesvilab/FragPipe

commit 1486d2e0994bdb9ec648a17b5e9081dc900a1bc7

When operating in headless mode, if an underlying task returns a non-zero exit code, capture it and use it in a System.exit() call.

MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/ProcessBuilderInfo.java
@@ -158,6 +158,11 @@ public class ProcessBuilderInfo {
         toConsole(Fragpipe.COLOR_RED_DARKEST, msg, true, console);
         // all the cleanup is done in the final block
       } finally {
+        // grab the exit code of the process to use for System.exit() if operating in headless mode
+        int overallExitCode = 0;
+        if (started != null && started.exitValue() != 0) {
+          overallExitCode = started.exitValue();
+        }
         // in the end whatever happens always try to kill the process
         if (started != null && started.isAlive()) {
           log.debug("Killing underlying external process");
@@ -167,6 +172,9 @@ public class ProcessBuilderInfo {
           pr.close();
         } catch (Exception e) {
           log.error("Error closing redirected std/err streams from external process", e);
+        }
+        if (Fragpipe.headless && overallExitCode != 0) {
+          System.exit(overallExitCode);
         }
       }
     };

commit 7e533e3ebe030ff3a798280a2728e5001fb8c0f3

Fix a bug that the tools aren't killed after clicking "stop" in GUI mode
Introduced in 1486d2e0994bdb9ec648a17b5e9081dc900a1bc7, https://github.com/Nesvilab/FragPipe/pull/1136

MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/ProcessBuilderInfo.java
@@ -150,13 +150,18 @@ public class ProcessBuilderInfo {
       } finally {
         // grab the exit code of the process to use for System.exit() if operating in headless mode
         int overallExitCode = 0;
-        if (started != null && started.exitValue() != 0) {
-          overallExitCode = started.exitValue();
+        try {
+          if (started != null && !started.isAlive() && started.exitValue() != 0) {
+            overallExitCode = started.exitValue();
+          }
+        } catch (Exception ex) {
+          log.debug("Could not get the exit code: {}", ex.getMessage());
         }
         // in the end whatever happens always try to kill the process
         if (started != null && started.isAlive()) {
           log.debug("Killing underlying external process");
           started.destroyForcibly();
+          overallExitCode = 1;
         }
         try {
           pr.close();