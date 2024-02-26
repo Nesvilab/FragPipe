f79d7f4462e87b43174a8eb1c1bbf85852b40509
Fengchao <fcyucn@gmail.com>
2024-02-26 15:06:22 -0500

    Upgrade MSBooster to 1.2.11

--- MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdMSBooster.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdMSBooster.java
@@ -47,7 +47,7 @@ public class CmdMSBooster extends CmdBase {
 
   private static final Logger log = LoggerFactory.getLogger(CmdMSBooster.class);
   public static String NAME = "MSBooster";
-  public static final String JAR_MSBOOSTER_NAME = "msbooster-1.2.2.jar";
+  public static final String JAR_MSBOOSTER_NAME = "MSBooster-1.2.11.jar";
   public static final String JAR_MSBOOSTER_MAIN_CLASS = "Features.MainClass";
   private static final String[] JAR_DEPS = {BATMASS_IO_JAR};
   private static final Pattern pattern1 = Pattern.compile("\\.pepXML$");
