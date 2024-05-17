95bf8cef798ec1a9658117aebe8b0850226d5903
Fengchao <fcyucn@gmail.com>
2024-05-17 15:52:43 -0400

    Update the Koina server tooltip.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/tools/msbooster/MSBoosterPanel.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/tools/msbooster/MSBoosterPanel.java
@@ -128,10 +128,10 @@ public class MSBoosterPanel extends JPanelBase {
     uiTextKoinaUrl.setColumns(20);
     FormEntry feKoinaUrl = mu.feb("koina-url", uiTextKoinaUrl)
         .label("Koina server URL: ")
-        .tooltip("Fill in your Koina server URL if you want to use the models in Koina.\nThe public one is https://koina.proteomicsdb.org/v2/models/")
+        .tooltip("Fill in your Koina server URL if you want to use the models in Koina.\nThe public one is https://koina.wilhelmlab.org:443/v2/models/")
         .create();
 
-    uiLabelKoinaUrl = createClickableHtml("Fill in your Koina server URL if you want to use the models in <a href=\"https://koina.wilhelmlab.org/\">Koina</a>. The public one is https://koina.proteomicsdb.org/v2/models/");
+    uiLabelKoinaUrl = createClickableHtml("Fill in your Koina server URL if you want to use the models in <a href=\"https://koina.wilhelmlab.org/\">Koina</a>. The public one is https://koina.wilhelmlab.org:443/v2/models/");
 
     uiComboRTModel = createUiCombo(new String[]{
         "DIA-NN",
