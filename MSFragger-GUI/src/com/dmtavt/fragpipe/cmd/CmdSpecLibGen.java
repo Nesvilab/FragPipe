Nesvilab/FragPipe

commit b06dac4e8341c54b3dc44a7d0b33d67f729560ae

Remove "make library skyline compatible" checkbox from the spec lib gen tab.

MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdSpecLibGen.java
@@ -181,7 +181,6 @@ public class CmdSpecLibGen extends CmdBase {
               max_delta_ppm,
               fragment_types.replace("'", "\\'"),
               speclibPanel.hasNeutralLoss() ? "--enable_unspecific_losses " : "",
-              speclibPanel.isConvertPSM() && speclibPanel.getEasyPQPignoreUnannotatedOption() ? "--ignore_unannotated " : "",
               speclibPanel.isConvertPSM() ? "--decoy_prefix " + decoyTag + " ": "",
               speclibPanel.isConvertPSM() && !speclibPanel.getEasypqpGlycoOption().isEmpty() ? "--labile_mods " + speclibPanel.getEasypqpGlycoOption() + " " : "",
               speclibPanel.isConvertPSM() ? String.format("--max_glycan_q %s ", speclibPanel.getEasypqpMaxGlycanQ()) : ""

MSFragger-GUI/src/com/dmtavt/fragpipe/tools/speclibgen/SpeclibPanel.java
@@ -285,9 +285,6 @@ public class SpeclibPanel extends JPanelBase {
     uiCheckNeutralLoss = new UiCheck("neutral loss", null, false);
     FormEntry feNeutralLoss = mu.feb(uiCheckNeutralLoss).name("easypqp.neutral_loss").label("neutral loss").tooltip("Add neutral loss fragments to the spectral library.").create();
 
-    checkIgnoreUnannotated = new UiCheck("Make library Skyline compatible", null, false);
-    FormEntry feIgnoreUnannotated = mu.feb(checkIgnoreUnannotated).name("easypqp.ignore_unannotated").label("Make library Skyline compatible").tooltip("Remove PSMs not matching Unimod modifications from input files prior to library building.\nMakes Skyline import easier if searches contain non-Unimod modifications (e.g., open or glyco search)").create();
-
     uiComboGlycoMode = UiUtils.createUiCombo(Arrays.asList("Regular (not glyco)", "O-glyco", "N-glyco", "N-glyco+HexNAc"));
     String glycoTooltip = "Labile glyco search modes:\n" +
             "Regular = standard, non-glyco search (all modifications included intact on fragment ions in the library)\n" +
@@ -332,8 +329,7 @@ public class SpeclibPanel extends JPanelBase {
     mu.add(p, feNeutralLoss.comp).wrap();
 
     mu.add(p, fe_max_delta_unimod.label(), mu.ccR());
-    mu.add(p, fe_max_delta_unimod.comp).split();
-    mu.add(p, feIgnoreUnannotated.comp).gapLeft("35").wrap();
+    mu.add(p, fe_max_delta_unimod.comp).wrap();
 
     mu.add(p, fe_max_delta_ppm.label(), mu.ccR());
     mu.add(p, fe_max_delta_ppm.comp).wrap();
@@ -515,9 +511,7 @@ public class SpeclibPanel extends JPanelBase {
   public boolean hasNeutralLoss() {
     return uiCheckNeutralLoss.isSelected();
   }
-  public boolean getEasyPQPignoreUnannotatedOption() {
-    return checkIgnoreUnannotated.isSelected();
-  }
+
   public boolean isConvertPSM() {
     return psmConvertButton.isSelected();
   }

commit 72984ef359559c1bc34b2b1b629ac4246e652c92

Fix a bug caused by b06dac4

MSFragger-GUI/src/com/dmtavt/fragpipe/cmd/CmdSpecLibGen.java
@@ -175,7 +175,7 @@ public class CmdSpecLibGen extends CmdBase {
       final String fragment_types = speclibPanel.getEasypqp_fragment_types(); // EasyPQP convert
       final double rt_lowess_fraction = speclibPanel.getEasypqpRTLowessFraction(); // EasyPQP library
 
-      cmd.add(OsUtils.asSingleArgument(String.format("--unimod %s --max_delta_unimod %s --max_delta_ppm %s --fragment_types %s %s%s%s%s%s",
+      cmd.add(OsUtils.asSingleArgument(String.format("--unimod %s --max_delta_unimod %s --max_delta_ppm %s --fragment_types %s %s%s%s%s",
               unimodPath.toAbsolutePath().toString().replace("\\", "/"),
               max_delta_unimod,
               max_delta_ppm,