039a3b0db3b66babf684cb0533a84fbcf36bdbf1
Fengchao <fcyucn@gmail.com>
2024-08-18 14:14:38 -0400

    Pick the latest version if there are multiple Philosopher versions in the directory.

--- MSFragger-GUI/src/com/dmtavt/fragpipe/FragPipeMain.java
+++ MSFragger-GUI/src/com/dmtavt/fragpipe/FragPipeMain.java
@@ -21,13 +21,20 @@ import static com.github.chhh.utils.OsUtils.isUnix;
 import static com.github.chhh.utils.OsUtils.isWindows;
 
 import com.github.chhh.utils.SwingUtils;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 public class FragPipeMain {
 
-  public static final String PHILOSOPHER_VERSION = "5.1.1";
+  public static String PHILOSOPHER_VERSION = "0.0.0";
+  private static final Pattern patternWin = Pattern.compile("philosopher-v(.+)\\.exe");
+  private static final Pattern patternUnix = Pattern.compile("philosopher-v((?!.*\\.exe$).+)");
 
   public static void main(String[] args) {
     if (args.length == 1 && (args[0].equalsIgnoreCase("--help") || args[0].equalsIgnoreCase("-h"))) {
@@ -69,23 +76,59 @@ public class FragPipeMain {
   }
 
   private static void getPhilosopherBin() {
-    String s;
-    if (isWindows()) {
-      s = "philosopher-v" + PHILOSOPHER_VERSION + ".exe";
-    } else if (isUnix()) {
-      s = "philosopher-v" + PHILOSOPHER_VERSION;
-    } else {
-      SwingUtils.showErrorDialog(null, "Philosopher only supports Windows and Unix systems", "Philosopher not supported in this OS");
+    Path p = FragpipeLocations.get().getDirTools().resolve("Philosopher");
+    DefaultArtifactVersion latestVersionWin = new DefaultArtifactVersion(PHILOSOPHER_VERSION);
+    DefaultArtifactVersion latestVersionUnix = new DefaultArtifactVersion(PHILOSOPHER_VERSION);
+    Path pWin = null;
+    Path pUnix = null;
+
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(p)) {
+      for (Path entry : stream) {
+        Matcher mWin = patternWin.matcher(entry.getFileName().toString());
+        Matcher mUnix = patternUnix.matcher(entry.getFileName().toString());
+        if (mWin.matches()) {
+          DefaultArtifactVersion v = new DefaultArtifactVersion(mWin.group(1));
+          if (latestVersionWin.compareTo(v) < 0) {
+            latestVersionWin = v;
+            pWin = entry;
+          }
+        }
+        if (mUnix.matches()) {
+          DefaultArtifactVersion v = new DefaultArtifactVersion(mUnix.group(1));
+          if (latestVersionUnix.compareTo(v) < 0) {
+            latestVersionUnix = v;
+            pUnix = entry;
+          }
+        }
+      }
+    } catch (IOException e) {
+      SwingUtils.showErrorDialogWithStacktrace(e, null);
       Fragpipe.philosopherBinPath = null;
+      PHILOSOPHER_VERSION = "NA";
       return;
     }
 
-    final Path p = FragpipeLocations.get().getDirTools().resolve("Philosopher").resolve(s).normalize();
-    if (Files.exists(p)) {
-      Fragpipe.philosopherBinPath = p.toAbsolutePath().toString();
+    if (isWindows()) {
+      if (pWin != null) {
+        Fragpipe.philosopherBinPath = pWin.normalize().toAbsolutePath().toString();
+        PHILOSOPHER_VERSION = latestVersionWin.toString();
+      } else {
+        SwingUtils.showErrorDialog(null, "Philosopher binary not found at " + p, "Philosopher not found");
+        Fragpipe.philosopherBinPath = null;
+        PHILOSOPHER_VERSION = "NA";
+      }
+    } else if (isUnix()) {
+      if (pUnix != null) {
+        Fragpipe.philosopherBinPath = pUnix.normalize().toAbsolutePath().toString();
+        PHILOSOPHER_VERSION = latestVersionUnix.toString();
+      } else {
+        Fragpipe.philosopherBinPath = null;
+        PHILOSOPHER_VERSION = "NA";
+      }
     } else {
-      SwingUtils.showErrorDialog(null, "Philosopher binary not found at " + p, "Philosopher not found");
+      SwingUtils.showErrorDialog(null, "Philosopher only supports Windows and Unix systems", "Philosopher not supported in this OS");
       Fragpipe.philosopherBinPath = null;
+      PHILOSOPHER_VERSION = "NA";
     }
   }
 }
