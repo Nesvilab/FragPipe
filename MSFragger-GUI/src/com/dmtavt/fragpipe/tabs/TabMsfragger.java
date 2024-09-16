Nesvilab/FragPipe

commit 3ac92d7a201ac21ac85c1d6e08efcf3c772c8c50

Use "@" to replace "-" for nonspecific and nocleavage enzymes.

MSFragger-GUI/src/com/dmtavt/fragpipe/tabs/TabMsfragger.java
@@ -817,11 +817,11 @@ public class TabMsfragger extends JPanelBase {
 
     uiTextEnzymeName = new UiText();
     FormEntry feEnzymeName = mu.feb(MsfraggerParams.PROP_search_enzyme_name_1, uiTextEnzymeName).label("Enzyme name 1").create();
-    uiTextCuts = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z-]").text("KR").create();
+    uiTextCuts = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z@]").text("KR").create();
     uiTextCuts.addFocusListener(enzymeSpecFocusListener);
     FormEntry feCuts = mu.feb(MsfraggerParams.PROP_search_enzyme_cut_1, uiTextCuts).label("Cuts 1")
         .tooltip("Capital letters for amino acids after which the enzyme cuts.").create();
-    uiTextNocuts = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z-]").text("P").create();
+    uiTextNocuts = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z@]").text("P").create();
     uiTextNocuts.addFocusListener(enzymeSpecFocusListener);
     FormEntry feNocuts = mu.feb(MsfraggerParams.PROP_search_enzyme_nocut_1, uiTextNocuts).label("No cuts 1")
         .tooltip("Amino acids before which the enzyme won't cut.").create();
@@ -838,11 +838,11 @@ public class TabMsfragger extends JPanelBase {
 
     uiTextEnzymeName2 = new UiText();
     FormEntry feEnzymeName2 = mu.feb(MsfraggerParams.PROP_search_enzyme_name_2, uiTextEnzymeName2).label("Enzyme name 2").create();
-    uiTextCuts2 = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z-]").text("KR").create();
+    uiTextCuts2 = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z@]").text("KR").create();
     uiTextCuts2.addFocusListener(enzymeSpecFocusListener2);
     FormEntry feCuts2 = mu.feb(MsfraggerParams.PROP_search_enzyme_cut_2, uiTextCuts2).label("Cuts 2")
         .tooltip("Capital letters for amino acids after which the enzyme cuts.").create();
-    uiTextNocuts2 = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z-]").text("P").create();
+    uiTextNocuts2 = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z@]").text("P").create();
     uiTextNocuts2.addFocusListener(enzymeSpecFocusListener2);
     FormEntry feNocuts2 = mu.feb(MsfraggerParams.PROP_search_enzyme_nocut_2, uiTextNocuts2).label("No cuts 2")
         .tooltip("Amino acids before which the enzyme won't cut.").create();

MSFragger-GUI/src/com/dmtavt/fragpipe/tools/fragger/EnzymeProvider.java
@@ -34,9 +34,9 @@ public class EnzymeProvider implements Supplier<List<MsfraggerEnzyme>> {
     if (props == null || props.isEmpty()) {
       throw new IllegalStateException("Could not load " + PROPERTIES_FN + " for MSFragger supported enzymes");
     }
-    final Pattern reCut = Pattern.compile("cut\\(([a-zA-Z-]+)\\)");
-    final Pattern reNocuts = Pattern.compile("nocuts\\(([a-zA-Z-]+)\\)");
-    final Pattern reSense = Pattern.compile("sense\\(([a-zA-Z-]+)\\)");
+    final Pattern reCut = Pattern.compile("cut\\(([A-Z@]+)\\)");
+    final Pattern reNocuts = Pattern.compile("nocuts\\(([A-Z@]+)\\)");
+    final Pattern reSense = Pattern.compile("sense\\(([CN])\\)");
     List<MsfraggerEnzyme> list = props.stringPropertyNames().stream().sorted().map(name -> {
       String val = props.getProperty(name);
       String cut = "";

commit ee388ec92c309a9d6db320eeff957cf6226530da

Fix a bug caused by 3ac92d7

MSFragger-GUI/src/com/dmtavt/fragpipe/tabs/TabMsfragger.java
@@ -817,11 +817,11 @@ public class TabMsfragger extends JPanelBase {
 
     uiTextEnzymeName = new UiText();
     FormEntry feEnzymeName = mu.feb(MsfraggerParams.PROP_search_enzyme_name_1, uiTextEnzymeName).label("Enzyme name 1").create();
-    uiTextCuts = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z@]").text("KR").create();
+    uiTextCuts = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z@]", "@").text("KR").create();
     uiTextCuts.addFocusListener(enzymeSpecFocusListener);
     FormEntry feCuts = mu.feb(MsfraggerParams.PROP_search_enzyme_cut_1, uiTextCuts).label("Cuts 1")
         .tooltip("Capital letters for amino acids after which the enzyme cuts.").create();
-    uiTextNocuts = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z@]").text("P").create();
+    uiTextNocuts = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z@]", "@").text("P").create();
     uiTextNocuts.addFocusListener(enzymeSpecFocusListener);
     FormEntry feNocuts = mu.feb(MsfraggerParams.PROP_search_enzyme_nocut_1, uiTextNocuts).label("No cuts 1")
         .tooltip("Amino acids before which the enzyme won't cut.").create();
@@ -838,11 +838,11 @@ public class TabMsfragger extends JPanelBase {
 
     uiTextEnzymeName2 = new UiText();
     FormEntry feEnzymeName2 = mu.feb(MsfraggerParams.PROP_search_enzyme_name_2, uiTextEnzymeName2).label("Enzyme name 2").create();
-    uiTextCuts2 = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z@]").text("KR").create();
+    uiTextCuts2 = UiUtils.uiTextBuilder().cols(6).filter("[^A-Z@]", "@").text("KR").create();
     uiTextCuts2.addFocusListener(enzymeSpecFocusListener2);
     FormEntry feCuts2 = mu.feb(MsfraggerParams.PROP_search_enzyme_cut_2, uiTextCuts2).label("Cuts 2")
         .tooltip("Capital letters for amino acids after which the enzyme cuts.").create();
-    uiTextNocuts2 = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z@]").text("P").create();
+    uiTextNocuts2 = UiUtils.uiTextBuilder().cols(3).filter("[^A-Z@]", "@").text("P").create();
     uiTextNocuts2.addFocusListener(enzymeSpecFocusListener2);
     FormEntry feNocuts2 = mu.feb(MsfraggerParams.PROP_search_enzyme_nocut_2, uiTextNocuts2).label("No cuts 2")
         .tooltip("Amino acids before which the enzyme won't cut.").create();

MSFragger-GUI/src/com/dmtavt/fragpipe/tools/fragger/fragger_nonspecific.params
@@ -48,7 +48,7 @@ diagnostic_fragments =
 remainder_fragment_masses =
 
 search_enzyme_name_1 = nonspecific
-search_enzyme_cut_1 = -
+search_enzyme_cut_1 = @
 search_enzyme_nocut_1 =
 search_enzyme_sense_1 = C
 allowed_missed_cleavage_1 = 2

MSFragger-GUI/src/com/github/chhh/utils/swing/DocumentFilters.java
@@ -38,27 +38,27 @@ public class DocumentFilters {
 
   private DocumentFilters() {}
 
-  /**
-   * Filter that removes all substrings matching the provided regular expression.
-   * @param filteredCharsRegex Regular expression for filtering out characters.
-   */
-  public static PlainDocument getFilter(final String filteredCharsRegex) {
+  public static PlainDocument getFilter(final String filteredCharsRegex, final String replacement) {
     PlainDocument doc = new PlainDocument();
     final Pattern regex = Pattern.compile(filteredCharsRegex);
     doc.setDocumentFilter(new DocumentFilter() {
       @Override
       public void insertString(DocumentFilter.FilterBypass fb, int off, String str, AttributeSet attr)
           throws BadLocationException {
-        fb.insertString(off, regex.matcher(str).replaceAll(""), attr);
+        fb.insertString(off, regex.matcher(str).replaceAll(replacement), attr);
       }
 
       @Override
       public void replace(DocumentFilter.FilterBypass fb, int off, int len, String str, AttributeSet attr)
           throws BadLocationException {
-        fb.replace(off, len, regex.matcher(str).replaceAll(""), attr);
+        fb.replace(off, len, regex.matcher(str).replaceAll(replacement), attr);
       }
     });
     return doc;
+  }
+
+  public static PlainDocument getFilter(final String filteredCharsRegex) {
+    return getFilter(filteredCharsRegex, "");
   }
 
   public static PlainDocument getDigitsOnlyFilter() {

MSFragger-GUI/src/com/github/chhh/utils/swing/UiUtils.java
@@ -100,9 +100,13 @@ public class UiUtils {
       return this;
     }
 
-    public UiTextBuilder filter(String filterRegex) {
-      uiText.setDocument(DocumentFilters.getFilter(filterRegex));
+    public UiTextBuilder filter(String filterRegex, String replacement) {
+      uiText.setDocument(DocumentFilters.getFilter(filterRegex, replacement));
       return this;
+    }
+
+    public UiTextBuilder filter(String filterRegex) {
+      return filter(filterRegex, "");
     }
 
     public UiTextBuilder text(String text) {

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-C57.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-DIA-Astral.workflow
@@ -157,7 +157,7 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
+msfragger.search_enzyme_cut_1=@
 msfragger.search_enzyme_cut_2=
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-DIA.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-TMT10.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-diaPASEF.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-glyco.workflow
@@ -159,7 +159,7 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
+msfragger.search_enzyme_cut_1=@
 msfragger.search_enzyme_cut_2=
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA-phospho.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/src/resources/workflows/Nonspecific-HLA.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/src/resources/workflows/Nonspecific-peptidome.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-HLA-C57.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-HLA-DIA-Astral.workflow
@@ -157,7 +157,7 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
+msfragger.search_enzyme_cut_1=@
 msfragger.search_enzyme_cut_2=
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null

MSFragger-GUI/workflows/Nonspecific-HLA-DIA.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-HLA-TMT10.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-HLA-diaPASEF.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-HLA-glyco.workflow
@@ -159,7 +159,7 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
+msfragger.search_enzyme_cut_1=@
 msfragger.search_enzyme_cut_2=
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null

MSFragger-GUI/workflows/Nonspecific-HLA-phospho.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-HLA.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=

MSFragger-GUI/workflows/Nonspecific-peptidome.workflow
@@ -157,8 +157,8 @@ msfragger.require_precursor=true
 msfragger.restrict_deltamass_to=all
 msfragger.reuse_dia_fragment_peaks=false
 msfragger.run-msfragger=true
-msfragger.search_enzyme_cut_1=-
-msfragger.search_enzyme_cut_2=-
+msfragger.search_enzyme_cut_1=@
+msfragger.search_enzyme_cut_2=@
 msfragger.search_enzyme_name_1=nonspecific
 msfragger.search_enzyme_name_2=null
 msfragger.search_enzyme_nocut_1=