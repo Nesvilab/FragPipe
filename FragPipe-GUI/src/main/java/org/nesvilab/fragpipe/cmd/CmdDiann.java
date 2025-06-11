87cfaf02e432eaaeda7b8c8af355192e1a02cd8b
Fengchao <fcyucn@gmail.com>
2025-06-11 17:16:34 -0400

    Upgrade LFQ site reporter to 1.0.11

--- FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/cmd/CmdDiann.java
+++ FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/cmd/CmdDiann.java
@@ -76,7 +76,7 @@ public class CmdDiann extends CmdBase {
   private static final String NAME = "DIA-Quant";
   private static final List<String> SUPPORTED_FORMATS_WIN = Arrays.asList("mzML", "d", "dia", "wiff", "raw");
   private static final List<String> SUPPORTED_FORMATS_LINUX = Arrays.asList("mzML", "d", "dia", "raw");
-  private static final String SITE_REPORTER = "LFQ-SiteReporter-1.0.10.jar";
+  private static final String SITE_REPORTER = "LFQ-SiteReporter-1.0.11.jar";
   public static final Pattern labelPattern = Pattern.compile("([A-Znc*]+)([\\d.+-]+)");
 
   private final String diannPath;
