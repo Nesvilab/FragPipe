2ec69f7f1b8f295d0faea05c6539702054d6e2df
Fengchao <fcyucn@gmail.com>
2025-11-21 15:35:01 -0500

    Upgrade MSBooster to 1.4.5

--- FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/cmd/CmdMSBooster.java
+++ FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/cmd/CmdMSBooster.java
@@ -48,7 +48,7 @@ public class CmdMSBooster extends CmdBase {
 
   private static final Logger log = LoggerFactory.getLogger(CmdMSBooster.class);
   public static String NAME = "MSBooster";
-  public static final String MSBOOSTER_VERSION = "1.4.3";
+  public static final String MSBOOSTER_VERSION = "1.4.5";
   public static final String JAR_MSBOOSTER_NAME = "MSBooster-" + MSBOOSTER_VERSION + ".jar";
   public static final String JAR_MSBOOSTER_MAIN_CLASS = "mainsteps.MainClass";
   public static final String[] JAR_DEPS = {BATMASS_IO_JAR};
