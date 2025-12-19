c134a50c272742faf3a60ec4902a2f150c7a2cfa
Fengchao <fcyucn@gmail.com>
2025-12-19 11:55:39 -0500

    Upgrade FP-PDV to 1.5.6

--- FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/tabs/TabRun.java
+++ FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/tabs/TabRun.java
@@ -92,7 +92,7 @@ public class TabRun extends JPanelWithEnablement {
   public static final String TAB_PREFIX = "tab-run.";
   private static final String LAST_WORK_DIR = "workdir.last-path";
   private static final String PROP_FILECHOOSER_LAST_PATH = TAB_PREFIX + "filechooser.last-path";
-  public static final String PDV_NAME = "/FP-PDV/FP-PDV-1.5.5.jar";
+  public static final String PDV_NAME = "/FP-PDV/FP-PDV-1.5.6.jar";
   public static final String FRAG_ANNOTATOR = "/FP-PDV/FragAnnotator-1.0.0.jar";
   public static final String GENERATE_REPORTS_NAME = "generate_reports_pdf.py";
   private static final String FRAGPIPE_ANALYST_URL = Fragpipe.propsFix().getProperty("fragpipe-analyst-url", "http://fragpipe-analyst.nesvilab.org/");
