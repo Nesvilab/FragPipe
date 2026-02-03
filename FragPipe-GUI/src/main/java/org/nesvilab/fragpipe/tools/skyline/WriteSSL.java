Nesvilab/FragPipe

commit db29977d56761fa627fb849d48cd017f1d5eb6d1

Fix a bug when using diaTracer+Skyline.

FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/tools/skyline/WriteSSL.java
@@ -28,7 +28,7 @@ public class WriteSSL {
      * file     scan    charge  sequence    score-type  score   RT  IM
      * Sequence includes all mods. IM is optional. Score-type can be PERCOLATOR QVALUE or PEPTIDE PROPHET SOMETHING
      */
-    public void writeSSL(Set<Path> psmtsvFiles, Path outputPath, boolean isPercolator, Set<String> lcmsFiles) throws IOException {
+    public void writeSSL(Set<Path> psmtsvFiles, Path outputPath, boolean isPercolator, Set<String> lcmsFiles, boolean isDia) throws IOException {
         ArrayList<String> output = new ArrayList<>();
 
         // map file paths to the file names that will be in the psm.tsv
@@ -37,7 +37,13 @@ public class WriteSSL {
             Path path = Paths.get(lcmsFile);
             String fileName = path.getFileName().toString();
             int dotIndex = fileName.lastIndexOf('.');
-            lcmsFileNames.put(fileName.substring(0, dotIndex), lcmsFile.replace("\\", "/").replaceFirst("\\.d$", "_uncalibrated.mzML"));
+            if (isDia) {
+                if (lcmsFile.endsWith(".d")) {
+                    lcmsFileNames.put(fileName.substring(0, dotIndex) + "_diatracer", lcmsFile.replace("\\", "/").replaceFirst("\\.d$", "_diatracer.mzML"));
+                }
+            } else {
+                lcmsFileNames.put(fileName.substring(0, dotIndex), lcmsFile.replace("\\", "/").replaceFirst("\\.d$", "_uncalibrated.mzML"));
+            }
         }
 
         boolean useIonQuantPeaks = true;

FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/tools/skyline/Skyline.java
@@ -183,9 +183,9 @@ public class Skyline {
         sslWriter = new WriteSSL();
         if (dataType.contentEquals("DIA")) {
           // psm.tsv refers to DDA files, need to provide as well
-          sslWriter.writeSSL(psmTsvFiles, sslPath, isPercolator, ddaAndDIAfiles);
+          sslWriter.writeSSL(psmTsvFiles, sslPath, isPercolator, ddaAndDIAfiles, dataType.equalsIgnoreCase("dia"));
         } else {
-          sslWriter.writeSSL(psmTsvFiles, sslPath, isPercolator, lcmsFiles);
+          sslWriter.writeSSL(psmTsvFiles, sslPath, isPercolator, lcmsFiles, dataType.equalsIgnoreCase("dia"));
         }
       }
 

commit 59a3e8d51945862c32238379e5b820f219ea2e99

Fix a bug caused by db29977d56761fa627fb849d48cd017f1d5eb6d1

FragPipe-GUI/src/main/java/org/nesvilab/fragpipe/tools/skyline/WriteSSL.java
@@ -40,6 +40,8 @@ public class WriteSSL {
             if (isDia) {
                 if (lcmsFile.endsWith(".d")) {
                     lcmsFileNames.put(fileName.substring(0, dotIndex) + "_diatracer", lcmsFile.replace("\\", "/").replaceFirst("\\.d$", "_diatracer.mzML"));
+                } else {
+                    lcmsFileNames.put(fileName.substring(0, dotIndex), lcmsFile.replace("\\", "/"));
                 }
             } else {
                 lcmsFileNames.put(fileName.substring(0, dotIndex), lcmsFile.replace("\\", "/").replaceFirst("\\.d$", "_uncalibrated.mzML"));